ec9d0a17f7897f9f252871f4e31948faf337cef8
German Geredus <ger382115@gmail.com>
2023-09-28 04:15:00 -0300

    agrego footer, form, animaciones, flechas y timer funcionando

--- index.js
+++
@@ -1,7 +0,0 @@
-const acordeon = document.getElementsByClassName('contenedor');
-
-for (i=0; i<acordeon.length; i++) {
-  acordeon[i].addEventListener('click', function () {
-    this.classList.toggle('activa')
-  })
-}
---
+++ js/index.js
@@ -0,0 +1,39 @@
+const endDate = new Date(2023, 10, 28, 9, 0, 0);
+
+function updateCountdown() {
+  const now = new Date();
+  const timeLeft = endDate - now;
+
+  if (timeLeft <= 0) {
+    document.getElementById('days').textContent = '00';
+    document.getElementById('hours').textContent = '00';
+    document.getElementById('minutes').textContent = '00';
+    document.getElementById('seconds').textContent = '00';
+  } else {
+    const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = formatNumber(days);
+    document.getElementById('hours').textContent = formatNumber(hours);
+    document.getElementById('minutes').textContent = formatNumber(minutes);
+    document.getElementById('seconds').textContent = formatNumber(seconds);
+  }
+}
+
+function formatNumber(number) {
+  return number.toString().padStart(2, '0');
+}
+
+setInterval(updateCountdown, 1000);
+updateCountdown();
+
+
+const acordeon = document.getElementsByClassName('contenedor');
+
+for (i = 0; i < acordeon.length; i++) {
+  acordeon[i].addEventListener('click', function () {
+    this.classList.toggle('activa')
+  })
+}
